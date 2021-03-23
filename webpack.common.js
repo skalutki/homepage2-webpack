4ed43993d799a86f20ecbd81f09a6ffc9e11e182
skalutki <skalutki@gmail.com>
2021-03-23 18:20:07 +0000

    add html and pictures to project & update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -83,10 +83,23 @@ module.exports = {
       filename: "index.html",
     }),
     new HtmlWebpackPlugin({
-      template: "./src/pages/another.html",
+      template: "./src/pages/html.html",
       inject: true,
       chunks: ["index", "another"],
-      filename: "another.html",
+      filename: "html.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/css.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "css.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/js.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "js.html",
+    }),
+    
   ],
 };
